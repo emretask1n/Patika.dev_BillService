bcab786d7138b23cd6928c4eed1c367f9fc3ffab
Emre Taşkın <emretaskn57@gmail.com>
2023-01-27 22:47:52 +0300

    Initial commit

---
+++ src/test/java/com/emretaskin/BillService/BillServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emretaskin.BillService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
