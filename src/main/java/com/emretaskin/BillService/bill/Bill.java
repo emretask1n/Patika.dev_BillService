bfb9be6be36c1faad87d851079770c1ae0ae1d2b
Emre Taşkın <emretaskn57@gmail.com>
2023-01-30 21:31:23 +0300

    Lombok dependency and auto creation for Date is added.

--- src/main/java/com/emretaskin/BillService/bill/Bill.java
+++ src/main/java/com/emretaskin/BillService/bill/Bill.java
@@ -2,12 +2,14 @@ package com.emretaskin.BillService.bill;
 
 import com.emretaskin.BillService.customer.Customer;
 import jakarta.persistence.*;
-
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "Bill")
 public class Bill {
 
@@ -16,80 +18,9 @@ public class Bill {
     private Integer amount;
     private String department;
     private String company;
-    private java.sql.Date billCreatedAt;
+    private java.util.Date billCreatedAt = new java.util.Date();
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
-    public Bill(Long billId, Integer amount, String department, String company, Date billCreatedAt, Customer customer) {
-        this.billId = billId;
-        this.amount = amount;
-        this.department = department;
-        this.company = company;
-        this.billCreatedAt = billCreatedAt;
-        this.customer = customer;
-    }
-
-    public Bill() {
-    }
-
-    public Long getBillId() {
-        return billId;
-    }
-
-    public void setBillId(Long billId) {
-        this.billId = billId;
-    }
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public Date getBillCreatedAt() {
-        return billCreatedAt;
-    }
-
-    public void setBillCreatedAt(Date billCreatedAt) {
-        this.billCreatedAt = billCreatedAt;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    @Override
-    public String toString() {
-        return "Bill{" +
-                "billId='" + billId + '\'' +
-                ", amount=" + amount +
-                ", department='" + department + '\'' +
-                ", company='" + company + '\'' +
-                ", billCreatedAt=" + billCreatedAt +
-                ", customer=" + customer +
-                '}';
-    }
 }
--- src/main/java/com/emretaskin/BillService/customer/Customer.java
+++ src/main/java/com/emretaskin/BillService/customer/Customer.java
@@ -2,12 +2,17 @@ package com.emretaskin.BillService.customer;
 
 import com.emretaskin.BillService.bill.Bill;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "Customer")
 public class Customer {
 
@@ -15,66 +20,10 @@ public class Customer {
     private Long customerId;
     private String customerName;
     private String customerSurname;
-    private java.sql.Date createdAt;
+    private java.util.Date createdAt = new java.util.Date();
     @OneToMany(
             mappedBy = "customer",
             cascade = CascadeType.ALL,
             orphanRemoval = true)
     private List<Bill> bills = new ArrayList<>();
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getCustomerSurname() {
-        return customerSurname;
-    }
-
-    public void setCustomerSurname(String customerSurname) {
-        this.customerSurname = customerSurname;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "customerId=" + customerId +
-                ", customerName='" + customerName + '\'' +
-                ", customerSurname='" + customerSurname + '\'' +
-                ", createdAt=" + createdAt +
-                '}';
-    }
-
-    public Customer(Long customerId, String customerName, String customerSurname, Date createdAt) {
-        this.customerId = customerId;
-        this.customerName = customerName;
-        this.customerSurname = customerSurname;
-        this.createdAt = createdAt;
-    }
-
-    public Customer() {
-    }
-
-    public void setBills(List<Bill> bills) {
-        this.bills = bills;
-    }
 }
